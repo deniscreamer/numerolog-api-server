a7fbbc0f34713058e78987e732966fd56e26fc83
Denis Screamer <46084060+deniscreamer@users.noreply.github.com>
2019-03-27 12:23:38 +0300

    add sms-gate for notifications to clients

--- cron-add-daytable.js
+++ cron-add-daytable.js
@@ -1,5 +1,6 @@
 let request = require('request-promise-native');
 let moment = require('moment');
+const fetch = require('node-fetch');
 
 const API_URL = 'http://api.numerolog-valeria.ru';
 
@@ -7,8 +8,7 @@ const requestOptions = {
   baseUrl: API_URL,
   uri: '',
   headers: {
-    'User-Agent':
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
     'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
     'X-Requested-With': 'XMLHttpRequest',
     Referer: API_URL,
@@ -18,10 +18,14 @@ const requestOptions = {
 request = request.defaults(requestOptions);
 moment.locale('ru');
 
-const usernameFromApi = 'admin';
-const passwordFromApi = 'admin123';
+const usernameFromApi = 'XXX';
+const passwordFromApi = 'XXX';
+
+const smsGateUsername = 'XXX';
+const smsGatePassword = 'XXX';
 
 main();
+sendSmsNotification();
 
 async function main() {
   let dayTables = await sendRequest('/daytable');
@@ -34,8 +38,8 @@ async function main() {
   } while (newDate.weekday() > 4); // only not weekend */
 
   newDate = newDate.add('1', 'days'); // all days on week
-  
-  templateDayTable.date = newDate.format();
+
+  templateDayTable.date = newDate.format('YYYY-MM-DD');
   templateDayTable.weekday = newDate.format('dddd');
 
   let response = await sendRequestPost('/daytable', templateDayTable);
@@ -43,8 +47,32 @@ async function main() {
   console.log(response);
 }
 
+async function sendSmsNotification() {
+  const needDay = moment(moment.now()).add('1', 'days').format('DD.MM.YYYY');
+  const toDay = moment(moment.now()).format('DD-MM-YYYY');
+
+  let clientsNeedNotification = await sendRequest('/orders?dateread=' + needDay);
+  if (clientsNeedNotification.length) {
+    for await (client of clientsNeedNotification) {
+      if (client.payed) {
+        let smsTimeToSend = moment(toDay + ' ' + client.timeat, 'DD-MM-YYYY HH:mm');
+        let smsTo = client.contacts;
+        let smsMessage =
+          `Здравствуйте! У Вас завтра консультация с нумерологом в ${client.timeat} по МСК. Подготовьте ручку и тетрадку.`;
+
+        fetch(
+            `https://smsc.ru/sys/send.php?login=${smsGateUsername}&psw=${smsGatePassword}&phones=${encodeURI(smsTo)}&time=${smsTimeToSend.format('DDMMYYHHmm')}&mes=${encodeURI(smsMessage)}`
+            )
+          .then(response => console.log('send to => ' + smsTo + ' => ' + smsTimeToSend.format('DDMMYYHHmm') + ' => ' +
+            smsMessage));
+      }
+    }
+  }
+}
+
 function sendRequest(url) {
   return request(url)
+    .auth(usernameFromApi, passwordFromApi, true)
     .then(res => res)
     .catch(err => {
       throw new Error('error: ' + err);
@@ -53,7 +81,10 @@ function sendRequest(url) {
 
 function sendRequestPost(url, data) {
   return request
-    .post(url, { resolveWithFullResponse: true, form: data })
+    .post(url, {
+      resolveWithFullResponse: true,
+      form: data
+    })
     .auth(usernameFromApi, passwordFromApi, true)
     .then(res => res.body.id)
     .catch(err => {
